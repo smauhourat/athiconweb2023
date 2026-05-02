e5035c9a578850a06feee17eb6878fe642687b3b
Santiago Mauhourat <santiagomauhourat@hotmail.com>
2026-05-02 15:28:18 -0300

    safety

--- js/main.js
+++ js/main.js
@@ -52,15 +52,13 @@
 
 
 
-    // Activate smooth scroll on page load with hash links in the url
+    // On page load with hash in URL, jump instantly so the spinner hides over the correct position
     $(document).ready(function () {
         if (window.location.hash) {
             var initial_nav = window.location.hash;
             if ($(initial_nav).length) {
-                var scrollto = $(initial_nav).offset().top; // - scrolltoOffset;
-                $('html, body').animate({
-                    scrollTop: scrollto
-                }, 1500, 'easeInOutExpo');
+                var scrollto = $(initial_nav).offset().top;
+                $('html, body').scrollTop(scrollto);
             }
         }
     });
